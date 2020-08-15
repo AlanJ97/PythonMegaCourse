15361c3f044d5067d362c67d70b9076372b27345
Alan <alannavaseg_02@hotmail.com>
2020-08-15 12:23:59 -0500

    Adding comparison words function and messages of errors

--- EnglishDictionary/dictionary.py
+++ EnglishDictionary/dictionary.py
@@ -1,18 +1,42 @@
+#LOADING MODULES AND PACKAGES
+from difflib import  get_close_matches
 import json
+#SETTING THE CLASS
 
+#LOADING FILES WITH WORDS
 data = json.load(open("data.json"))
 
-def search_word(word):    
+#FUNCTION THAT SEARCHES FOR THE GIVEN WORD
+def search_word(word):   
     try:
-        
-        searched_word = data[word.lower()]
+        word = word.lower()
+        if word in data:
+            searched_word = data[word]
+        elif len(get_close_matches(word, data.keys())) > 0:
+            print( "Did you mean one of these options: {} ,  Which one? Or something different?".format(get_close_matches(word, data.keys())))
+            right_word = "trying one more time"
+            while right_word not in data:
+                right_word = input("Give me your right option, dude ")
+            return data[right_word]
+        else:
+            return "Your word does not exist in the records. Try with another"
+
     except:
-        return("Your word does nor exist in the records. Try with another")
+        return("Something went wrong with the program. Try once more later")
     return searched_word    
 
+#CYCLE THAT EXECUTES THE PROGRAM
 while True:
-    word_to_search_for = input("What's your word?")
+    print(" To finish the program enter 'salir' ")
+    word_to_search_for = input("What's your word? ")
     if word_to_search_for != "salir":
-        print(search_word(word_to_search_for))
+        output = search_word(word_to_search_for.strip())
+        print("")
+        if type(output) == list:
+            for item in output:
+                print(item)
+        else:
+            print(output)
+        print("")
     else: 
         break
