1c2f4ef3785b82fd7efc6ee59f2c51724fc79b40
Alan <alannavaseg_02@hotmail.com>
2020-08-22 17:56:55 -0500

    AGregando diferentes colores al mapa creado

---
+++ VolcanoAndPopulation/map1.py
@@ -0,0 +1,24 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+
+
+def color_points(elevation):
+    if int(elevation) < 2300:
+        return "orange"
+    else:
+        return "green"
+
+
+map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name = "My Map")
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon = folium.Icon(color="green"))
+map.add_child(fg)
+map.save("Map1.html")
