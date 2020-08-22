e072873e2e07f852e288e930856e47c816e5cda1
Alan <alannavaseg_02@hotmail.com>
2020-08-22 18:04:00 -0500

    CAda altura de volcan da un color distinti

--- VolcanoAndPopulation/map1.py
+++ VolcanoAndPopulation/map1.py
@@ -9,8 +9,10 @@ elev = list(data["ELEV"])
 
 
 def color_points(elevation):
-    if int(elevation) < 2300:
+    if elevation < 1000:
         return "orange"
+    elif 1001<= elevation <= 3000:
+        return "blue"
     else:
         return "green"
 
@@ -19,6 +21,6 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name = "My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon = folium.Icon(color="green"))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon = folium.Icon(color=color_points(el))))
 map.add_child(fg)
 map.save("Map1.html")
