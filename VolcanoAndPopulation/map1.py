d4b1b646e7868830fbc329be360167e3d06a0d20
Alan <alannavaseg_02@hotmail.com>
2020-08-22 19:57:50 -0500

    Agregando colores por poblacion

--- VolcanoAndPopulation/map1.py
+++ VolcanoAndPopulation/map1.py
@@ -21,6 +21,12 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name = "My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon = folium.Icon(color=color_points(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=el,
+        fill_color=color_points(el), color="gray", fill_opacity = 0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json','r', encoding="utf-8-sig").read(),
+    style_function=lambda x:{ 'fillColor':'yellow' if x['properties']['POP2005'] < 10000000 
+    else 'orange'  if 10000001 <= x['properties']['POP2005'] < 20000000 else 'red' }))
+
 map.add_child(fg)
 map.save("Map1.html")
